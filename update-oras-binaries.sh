f374a3ed083c30d465cae5fc55bd56eb9562f4c8
Ali <alishah.ece2015@gmail.com>
2025-04-20 19:30:18 -0400

    Implement structural updates and optimizations across multiple modules

--- update-oras-binaries.sh
+++ update-oras-binaries.sh
@@ -14,10 +14,13 @@ mkdir -p "$BIN_DIR"
 
 LATEST_VERSION="v1.3.0-beta.2"
 
+# Remove leading 'v' for filenames
+VERSION_NO_V="${LATEST_VERSION#v}"
+
 echo "Latest ORAS version: $LATEST_VERSION"
 
 # 2. Download binaries
-BASE_URL="https://github.com/oras-project/oras/releases/download/v$LATEST_VERSION"
+BASE_URL="https://github.com/oras-project/oras/releases/download/$LATEST_VERSION"
 
 PLATFORMS=(
   "windows_amd64"
@@ -27,11 +30,11 @@ PLATFORMS=(
   "darwin_arm64"
 )
 FILES=(
-  "oras_${LATEST_VERSION}_windows_amd64.zip"
-  "oras_${LATEST_VERSION}_linux_amd64.tar.gz"
-  "oras_${LATEST_VERSION}_linux_arm64.tar.gz"
-  "oras_${LATEST_VERSION}_darwin_amd64.tar.gz"
-  "oras_${LATEST_VERSION}_darwin_arm64.tar.gz"
+  "oras_${VERSION_NO_V}_windows_amd64.zip"
+  "oras_${VERSION_NO_V}_linux_amd64.tar.gz"
+  "oras_${VERSION_NO_V}_linux_arm64.tar.gz"
+  "oras_${VERSION_NO_V}_darwin_amd64.tar.gz"
+  "oras_${VERSION_NO_V}_darwin_arm64.tar.gz"
 )
 
 for i in "${!PLATFORMS[@]}"; do
@@ -43,12 +46,12 @@ done
 
 # 3. Update version in package.json
 if command -v jq >/dev/null 2>&1; then
-  jq --arg v "$LATEST_VERSION" '.version = $v' package.json > package.tmp.json && mv package.tmp.json package.json
-  echo "Updated package.json to version $LATEST_VERSION"
+  jq --arg v "$VERSION_NO_V" '.version = $v' package.json > package.tmp.json && mv package.tmp.json package.json
+  echo "Updated package.json to version $VERSION_NO_V"
 else
   # Fallback: sed (will only work if version is on its own line)
-  sed -i.bak -E "s/\"version\": *\"[^\"]+\"/\"version\": \"$LATEST_VERSION\"/" package.json
-  echo "Updated package.json to version $LATEST_VERSION (with sed)"
+  sed -i.bak -E "s/\"version\": *\"[^\"]+\"/\"version\": \"$VERSION_NO_V\"/" package.json
+  echo "Updated package.json to version $VERSION_NO_V (with sed)"
 fi
 
 echo "Done."
